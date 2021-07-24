85533c2cf433f218eafeb6fa739d96b853705f57
ArakelTheDragon@gmail.com <ArakelTheDragon@gmail.com>
2021-07-24 05:16:25 +0200

    Changing of the names

--- utils/setup.py
+++ utils/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
     
 setuptools.setup(
-    name="ciirc-utils",
+    name="utils",
     version="0.0.2",
     author="ArakelTheDragon",
     author_email="ArakelTheDragon@gmail.com",
